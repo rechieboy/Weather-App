695ea53e25fe586c3ba6359037487dc1164618cd
Rechie Lagria <rechielagria@gmail.com>
2018-03-26 02:22:06 +0800

    Final Commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,72 @@ import React, { Component } from 'react';
 import Titles from "./Components/Titles";
 import Form from "./Components/Form";
 import Weather from "./Components/Weather";
-import './App.css';
 
 
 const API_KEY = '2297341e552a6cf9736f3ab3d19248c3';
 
 class App extends Component {
+ state = {
+ 	temperature:undefined,
+ 	city:undefined,
+ 	country:undefined,
+ 	humidity:undefined,
+ 	description:undefined,
+ 	error:undefined
+ }
  getWeather = async (e) => {
  	e.preventDefault();
- 	const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}&units=metric`);
+ 	const city = e.target.elements.city.value;
+ 	const country = e.target.elements.country.value;
+ 	const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
  	const data = await api_call.json();
- 	console.log(data);
+
+ 	if(city && country){
+ 		this.setState({
+ 		temperature:data.main.temp,
+ 		city:data.name,
+ 		country:data.sys.country,
+ 		humidity:data.main.humidity,
+ 		description:data.weather[0].description,
+ 		error:""
+ 	 });
+ 	}else{
+ 		this.setState({
+ 		temperature:undefined,
+ 		city:undefined,
+ 		country:undefined,
+ 		humidity:undefined,
+ 		description:undefined,
+ 		error:"Please Enter the Values"
+ 	 });
+ 	}
+ 	
  }
  render() {
     return (
-      <div className="App">
-       <Titles/>
-       <Form getWeather={this.getWeather}/>
-       <Weather/>
+      <div>
+       <div className="wrapper">
+       	<div className="main">
+       		<div className="container">
+       			<div className="row">
+       				<div className="col-xs-5 title-container">
+       					  <Titles/>
+       				</div>
+       				<div className="col-xs-7 form-container">
+       					  <Form getWeather={this.getWeather}/>
+					       <Weather 
+					       temperature={this.state.temperature}
+					       city={this.state.city}
+					       country={this.state.country}
+					       humidity={this.state.humidity}
+					       description={this.state.description}
+					       error={this.state.error}
+					       />
+       				</div>
+       			</div>
+       		</div>
+       	</div>
+       </div>
       </div>
     );
   }
--- src/Components/Titles.js
+++ src/Components/Titles.js
@@ -1,14 +1,11 @@
 import React from 'react';
 
-class Titles extends React.Component{
-	render(){
-		return(
-			<div>
-				<h1>Weather Finder</h1>
-				<p>Find out Temperature, conditions and more...</p>
-			</div>
-		);
-	}
-}
+const Titles = () => (
+
+	<div>
+			<h1 className ="title-container__title" >Weather Finder</h1>
+			<h3 className ="title-container__subtitle">Find out Temperature, conditions and more...</h3>
+	</div>
+);
 
 export default Titles;
